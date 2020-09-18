3edc4ab1f4c81e56048ab0525988e2bc536d0c0e
Mikael Pajari <mikael.pajari@live.se>
2020-09-18 17:29:51 +0200

    grunden snart klar

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TiTaTo
+{
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            char[,] Board = new char[3,3]; // detta är kärnan av indexeringen av spel rutorna
+            Board[0,0] = 'x';
+            Board[2,2] = 'O';
+            intro();
+            print(Board);
+            
+            System.Console.Write("print a row  number: ");
+            int row = Convert.ToInt32(Console.ReadLine());
+            System.Console.Write("print a colum  number: ");
+            int colums = Convert.ToInt32(Console.ReadLine());
+           Board[row,colums] = 'X';
+           print(Board);
+
+
+            
+        }
+         public static void intro()
+            {
+            Console.WriteLine("---------------------");
+            Console.WriteLine("welcome to tic-tac-toe");
+            Console.WriteLine("--------------------");
+            }
+            
+            static void print(char[,]Board)
+            {
+                for (int row = 0; row < 3; row++) // denna 4loopen skriver ut all resultat 
+                {
+                    Console.Write("| ");
+                        for (int colums = 0; colums < 3; colums++)
+                        {
+                            Console.Write(Board[row, colums]);
+                            Console.Write(" | ");
+                        }
+                        Console.WriteLine();
+                }
+                
+            }
+    }
+        
+           
+    
+}
