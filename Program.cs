5c29e776e9fbefbe147295a1ac8bbd87d4ac35f1
Mikael Pajari <mikael.pajari@live.se>
2020-09-19 13:13:31 +0200

    uppdaterad

--- Program.cs
+++ Program.cs
@@ -8,21 +8,41 @@ namespace TiTaTo
         
         static void Main(string[] args)
         {
+            char Player = 'X';
             char[,] Board = new char[3,3]; // detta är kärnan av indexeringen av spel rutorna
-            Board[0,0] = 'x';
-            Board[2,2] = 'O';
-            intro();
-            print(Board);
+           
             
+            intro();
+           bool Wincondition = true;
+            while (Wincondition == true)
+            {
+           
+            print(Board);
             System.Console.Write("print a row  number: ");
             int row = Convert.ToInt32(Console.ReadLine());
             System.Console.Write("print a colum  number: ");
             int colums = Convert.ToInt32(Console.ReadLine());
-           Board[row,colums] = 'X';
-           print(Board);
-
+            
+             Board[row,colums] = Player;
+           
+            Player = whosturn(Player);
+           
+           
+           
+            }
 
             
+        }
+        public static char whosturn(char currentplayer)
+        {
+             if (currentplayer == 'X')
+           {
+               return 'O'; 
+           }else 
+                {
+                    return 'X'; 
+                }
+
         }
          public static void intro()
             {
@@ -33,13 +53,14 @@ namespace TiTaTo
             
             static void print(char[,]Board)
             {
+                System.Console.WriteLine("   0   1   2   ");
                 for (int row = 0; row < 3; row++) // denna 4loopen skriver ut all resultat 
                 {
-                    Console.Write("| ");
+                    Console.Write(row + "| ");
                         for (int colums = 0; colums < 3; colums++)
                         {
                             Console.Write(Board[row, colums]);
-                            Console.Write(" | ");
+                            Console.Write(  " | ");
                         }
                         Console.WriteLine();
                 }
